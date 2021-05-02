d9be7b882167c26bcf6df2ab1858ef534946b45a
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-05-02 15:19:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -27,7 +27,7 @@ class CashRegister
   end    
     
   def apply_discount
-    self.total -= @total - @discount / 100 
+    @total -= @total - @discount / 100 
     if discount > 0.00 
       puts "After the discount, the total comes to $#{self.total}"
     end 
