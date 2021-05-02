337a6746321bb54a5ebca2073aa29940f9969e86
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-05-02 15:42:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,7 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
     quantity.times do 
-      @tital << title 
+      @item << title 
     end 
     @total += price * quantity
     @last_total = @total 
