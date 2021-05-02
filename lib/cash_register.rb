3eeec0562ee8715d21f0e4c48635a0f5a4ade67a
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-05-02 14:51:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,44 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :item, :total, :quantity, :discount
+  
+  @@total_cost = Hash.new {|h,k| h[k] = []}  
+  
+  def initialize(discount = 0.00)
+    @item = item 
+    @total = 0.00  
+    @discount = discount  
+    @quantity = quantity 
+    
+    
+  end
+  
+  def self.all  
+    @@total_cost
+  end 
+  
+  def total 
+      
+  end
+  
+  
+  def add_item(title, price, quantity = 1)
+    @@total_cost.store(title, price)
+    price = quantity * price 
+    self.total += price
+  end    
+    
+  def apply_discount
+    self.total -= (0.01 * @discount * @total)
+    if discount > 0.00 
+      puts "After the discount, the total comes to $#{self.total}"
+    end 
+  end 
+
+  def total 
+    @@total_cost << self.total 
+  end 
+  
+  
+end 
